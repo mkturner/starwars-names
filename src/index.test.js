1006cca857c4cf4d0209e30bae67892a660257e5
Marvin T <contact@marvinkturner.com>
2020-12-29 23:43:46 -0500

    test: add first test

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const starWars = require('./index');
+
+describe('starwars-names', function () {
+  it('should work!', function () {
+    expect(true).to.be.true;
+  });
+});
